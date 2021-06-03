b49ceef6f3c629fa66497373f985f03ad5f02203
Sumit Chutani <sumitengineer23@gmail.com>
2021-06-03 18:03:11 +0530

    Modified dialog controller file

--- src/application/DialogController.java
+++ src/application/DialogController.java
@@ -1,5 +1,36 @@
 package application;
 
-public class DialogController {
+import java.time.LocalDate;
 
+import datamodel.TodoData;
+import datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class DialogController {
+	@FXML
+	private TextField shortDescriptionField;
+	@FXML
+	private TextArea detailsArea;
+	@FXML
+	private DatePicker deadlinePicker;
+
+	public TodoItem processResults() {
+		String description = shortDescriptionField.getText().trim();
+		String details = detailsArea.getText().trim();
+		LocalDate deadlineValue = deadlinePicker.getValue();
+
+		TodoItem newItem = new TodoItem(description, details, deadlineValue);
+		TodoData.getInstance().addTodoItem(newItem);
+		return newItem;
+	}
+	
+	public void editResults(TodoItem item) {
+		shortDescriptionField.setText(item.getShortDescription());
+		detailsArea.setText(item.getDetails());
+		deadlinePicker.setValue(item.getDeadline());
+		TodoData.getInstance().deleteTodoItem(item);
+	}
 }
